82e3e9fdd82f65c8a899d23ff944f8f0a092e837
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-09-22 20:33:19 -0300

    Add files via upload

---
+++ Ex1/Ex1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valor1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.valor2 = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // valor1
+            // 
+            this.valor1.Location = new System.Drawing.Point(187, 154);
+            this.valor1.Name = "valor1";
+            this.valor1.Size = new System.Drawing.Size(112, 20);
+            this.valor1.TabIndex = 0;
+            this.valor1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(277, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ÁREA DO RETÂNGULO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // valor2
+            // 
+            this.valor2.Location = new System.Drawing.Point(488, 154);
+            this.valor2.Name = "valor2";
+            this.valor2.Size = new System.Drawing.Size(100, 20);
+            this.valor2.TabIndex = 2;
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.Location = new System.Drawing.Point(242, 361);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(219, 24);
+            this.resultado.TabIndex = 3;
+            this.resultado.Text = "A área do retângulo é:";
+            this.resultado.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(332, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(218, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Base";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(361, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 55);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(509, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Altura";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.valor2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox valor1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox valor2;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Ex1/Ex1/Retângulo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Retângulo
+    {
+        int baze;
+        int alt;
+        int area;
+
+        public Retângulo()
+        {
+            this.baze = 0;
+            this.alt = 0;
+            this.area = 0;
+        }
+
+
+
+        public void setBase(int baze)
+        {
+            this.baze = baze;
+        }
+
+
+
+
+        public int getBase()
+        {
+            return this.baze;
+        }
+
+        public void setAltura(int alt)
+        {
+            this.alt = alt;
+        }
+
+
+
+
+        public int getAltura()
+        {
+            return this.baze;
+        }
+
+
+
+        public int getArea()
+        {
+            return this.area;
+        }
+
+        public void calcularArea()
+        {
+            this.area = this.baze * this.alt;
+        }
+    }
+}
